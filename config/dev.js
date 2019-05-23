62587c2fd98c2a750532a2bf922d92194b29b621
Domagoj Majcenic <dmajcenic@gmail.com>
2019-05-23 21:48:08 +0200

    add hacky hashing

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://admin:administrator1@ds121182.mlab.com:21182/blog_dev2',
   cookieKey: '123123123'
 };
